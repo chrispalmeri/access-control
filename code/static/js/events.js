5c71e812bd3f2b42ad3e887cf1b8b6c76be9dc56
Chris Palmeri <chrispalmeri@gmail.com>
2023-06-21 19:32:06 -0500

    Less indentation

--- code/static/js/events.js
+++ code/static/js/events.js
@@ -1,27 +1,29 @@
 // events.js
 
-export default {
-    get: function() {
-        fetch('/api/events?channel=DEBUG&channel=INFO&channel=WARNING&limit=15')
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            var html = '';
-            if(data.length > 0) {
-                html = '<table><tr><th>Time</th><th>Channel</th><th>Message</th></tr>';
-                for (var event of data) {
-                    html += '<tr>';
-                    html += '<td>' + new Date(event.time).toLocaleString() + '</td>';
-                    html += '<td>' + event.channel + '</td>';
-                    html += '<td>' + event.message + '</td>';
-                    html += '</tr>';
-                }
-                html += '</table>';
-            } else {
-                html += 'No events found';
+function get() {
+    fetch('/api/events?channel=DEBUG&channel=INFO&channel=WARNING&limit=15')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        var html = '';
+        if(data.length > 0) {
+            html = '<table><tr><th>Time</th><th>Channel</th><th>Message</th></tr>';
+            for (var event of data) {
+                html += '<tr>';
+                html += '<td>' + new Date(event.time).toLocaleString() + '</td>';
+                html += '<td>' + event.channel + '</td>';
+                html += '<td>' + event.message + '</td>';
+                html += '</tr>';
             }
-            document.getElementById('event_list').innerHTML = html;
-        });
-    }
+            html += '</table>';
+        } else {
+            html += 'No events found';
+        }
+        document.getElementById('event_list').innerHTML = html;
+    });
+}
+
+export default {
+    get: get
 }
