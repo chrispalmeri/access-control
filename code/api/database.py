d3f1f9f38558dc27218ba332f11634f6dda48088
Chris Palmeri <chrispalmeri@gmail.com>
2023-06-25 16:39:46 -0500

    Improve database recovery

--- code/api/database.py
+++ code/api/database.py
@@ -5,7 +5,9 @@ class view(web.View):
     """For /database routes"""
 
     async def get(self):
-        return web.FileResponse(path=config.dbpath)
+        resp = web.FileResponse(path=config.dbpath)
+        resp.content_type = 'application/vnd.sqlite3'
+        return resp
 
     async def post(self):
         # not ideal https://docs.aiohttp.org/en/stable/web_quickstart.html#file-uploads
@@ -15,8 +17,16 @@ class view(web.View):
         newfile = data['file'].file
         filename = data['file'].filename
 
+        # check data starts with 'SQLite format 3'
+        start = newfile.read(15).decode('utf-8')
+        newfile.seek(0) # reset to beginning of file
+
+        if start != 'SQLite format 3':
+            #raise web.HTTPBadRequest(body=None, content_type=None) #400
+            return web.json_response({'error': 'not a sqlite file'}, status=400)
+
         # save it to filesystem
         with open(config.dbpath, 'wb') as oldfile:
             oldfile.write(newfile.read())
 
-        return web.Response(text='Restored {}'.format(filename))
+        return web.json_response({'success': 'Restored from {}'.format(filename)})
--- code/broadcast.py
+++ code/broadcast.py
@@ -7,14 +7,35 @@ def setup(parent):
     global app
     app = parent
 
-async def event(channel, message):
+def saveMessage(channel, message):
     conn.execute("""INSERT INTO events (time, channel, message)
         VALUES (?,?,?)""", (
             datetime.utcnow().isoformat(timespec='milliseconds') + 'Z',
             channel,
             message
-        ))
+        )
+    )
     conn.commit()
+
+def createTable():
+    conn.execute("""CREATE TABLE IF NOT EXISTS events (
+        id INTEGER PRIMARY KEY,
+        time TEXT,
+        channel TEXT,
+        message TEXT
+    )""")
+    saveMessage('DEBUG', 'Created events table')
+
+async def event(channel, message):
+    try:
+        saveMessage(channel, message)
+    except Exception as e:
+        # Exception > sqlite3.Error > sqlite3.DatabaseError > sqlite3.OperationalError
+        if str(e) == 'no such table: events':
+            createTable()
+            saveMessage(channel, message)
+        else:
+            raise e
 
     # Ping websockets about update
     if app:
